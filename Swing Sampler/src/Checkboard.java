452f38e6921bc3f36e132195aca012f5ad9b523e
dunham.perry <dunham.perry@MU-C1-09.mullen.pvt.k12.co.us>
2017-02-06 08:28:08 -0700

    Checkboard with for loop

--- Swing Sampler/src/Checkboard.java
+++ Swing Sampler/src/Checkboard.java
@@ -21,161 +21,23 @@ public class Checkboard extends Canvas
  
     	public void paint(Graphics graphics) 
     		{
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(0, 0, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(0, 20, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(0, 40, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(0, 60, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(0, 80, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(0, 100, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(0, 120, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(0, 140, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(20, 0, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(20, 20, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(20, 40, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(20, 60, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(20, 80, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(20, 100, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(20, 120, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(20, 140, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(40, 0, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(40, 20, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(40, 40, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(40, 60, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(40, 80, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(40, 100, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(40, 120, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(40, 140, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(60, 0, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(60, 20, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(60, 40, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(60, 60, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(60, 80, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(60, 100, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(60, 120, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(60, 140, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(80, 0, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(80, 20, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(80, 40, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(80, 60, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(80, 80, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(80, 100, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(80, 120, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(80, 140, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(100, 0, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(100, 20, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(100, 40, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(100, 60, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(100, 80, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(100, 100, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(100, 120, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(100, 140, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(120, 0, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(120, 20, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(120, 40, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(120, 60, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(120, 80, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(120, 100, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(120, 120, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(120, 140, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(140, 0, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(140, 20, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(140, 40, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(140, 60, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(140, 80, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(140, 100, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(140, 120, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(140, 140, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(160, 0, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(160, 20, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(160, 40, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(160, 60, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(160, 80, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(160, 100, 20, 20);
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(160, 120, 20, 20);
-	        graphics.setColor(Color.white);
-	        graphics.fillRect(160, 140, 20, 20);
-    		}
-    	
-    		public void delay()
-    			{
-    	        try
-    					{
-    					Thread.sleep(50);
-    					} catch (InterruptedException e)
-    					{
-    					e.printStackTrace();
+    			int counter = 1;
+    			for (int x = 0; x < 140; x = x + 20){
+    				if (counter == 15){ 
+    				counter = 1;
+    				}
+    				for (int y = 0; y < 140; y = y + 20){
+    					if (counter == 1 || counter == 3 || counter == 5 || counter == 7 || counter == 9 || counter == 11 || counter == 13 || counter == 15) {
+    					graphics.setColor(Color.black);
+    			        graphics.fillRect(x, y, 20, 20);
     					}
+    					else {
+        					graphics.setColor(Color.red);
+        			        graphics.fillRect(x, y, 20, 20);
+        					}
+    					System.out.println(counter);
+    			        counter++;
+    				}
     			}
+    		}
     	}
-	
